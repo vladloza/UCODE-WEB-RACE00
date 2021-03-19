b67fe849453889229034b218c551d8fac7d3a44b
Vladyslav Loza <vloza@c1r10p2.khpi.ucode-connect.study>
2021-03-19 13:37:43 +0200

    clear button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const expressionText = document.getElementById('history');
 const resultText = document.getElementById('result');
 function input(str) {
-    expressionText.value += str; 
+    if(str === '') {
+        expressionText.value = '0.0';
+        resultText.value = '0.0';
+    }
+    else expressionText.value += str;
+}
+function countResult() {
+    if(eval(expressionText.value) == undefined) expressionText.value = '0.00';
+    resultText.value = eval(expressionText.value);
+    expressionText.value = eval(expressionText.value);
 }
