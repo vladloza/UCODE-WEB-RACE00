ebd54a375acd436190530278815b9e49930cfa15
mburenko <halnuee@gmail.com>
2021-03-19 15:10:01 +0200

    Add function parseNumToNSystem

--- js/script.js
+++ js/script.js
@@ -1,14 +1,57 @@
-const expressionText = document.getElementById('history');
-const resultText = document.getElementById('result');
-function input(str) {
-    if(str === '') {
-        expressionText.value = '0.0';
-        resultText.value = '0.0';
+let $history = document.getElementsByClassName("history")[0];
+let $result = document.getElementsByClassName("result")[0];
+
+function insert(number) {
+    let $result = document.getElementsByClassName("result")[0];
+    console.log(number);
+    console.log($result.value);
+    if ($result.value == 0.00) {
+        $result.value = "";
     }
-    else expressionText.value += str;
+    $result.value += number;
+
 }
-function countResult() {
-    if(eval(expressionText.value) == undefined) expressionText.value = '0.00';
-    resultText.value = eval(expressionText.value);
-    expressionText.value = eval(expressionText.value);
+
+let operat;
+
+function operation(value) {
+    operat = value;
+    let x = !$history.textContent.slice(-1);
+    console.log(x);
+    if ($result.value == 0) {
+        $result.value = "";
+    } else if (x !== "+" && x !== "*" && x !== "-" && x !== "%" && x !== "÷") {
+        if ($history.value == 0.00) {
+            $history.value = "";
+        }
+        $history.value += $result.value;
+        $history.value += value;
+        $result.value = "";
+    }
+}
+
+function percent() {
+    $result.value = Number($result.value) / 100;
+    $history.value = "";
+}
+
+function plus_and_min() {
+    $history.value = "";
+    $result.value = (-1) * Number($result.value);
+}
+
+function clean() {
+    $history.value = "0.00";
+    $result.value = "0.00";
+}
+
+function answer() {
+    $history.value += $result.value;
+    console.log(eval($history.value));
+    $result.value = Number(eval($history.value));
+    $history.value = "";
+}
+
+function parseNumToNSystem(n) {
+    $result.value = parseInt($result.value).toString(n)
 }
